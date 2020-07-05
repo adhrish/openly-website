d3ae0df92f1d213a861b51814978b29be3637e6b
adh.rishabh <adh.rishabh@outlook.com>
2020-07-05 23:02:36 +0530

    update logo

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ if( /iPhone/i.test(navigator.userAgent) ) {
 
 function scrollheader(){
 		if(document.body.scrollTop <= 40){
+		document.getElementById("logotextopenly").style.fontSize= "1.8vw";
 		document.getElementById("header").style.height = "8vw";
 		document.getElementById("header").style.boxShadow="none";
 		document.getElementById("header-logo").style.height="5vw";
 		}
 		else if (document.body.scrollTop >= 40){
+			document.getElementById("logotextopenly").style.fontSize= "0.9vw";
 			document.getElementById("header").style.height = "3.5vw";
 			document.getElementById("header").style.boxShadow="0 0 1vw rgba(0,0,0,0.05)";
 			document.getElementById("header-logo").style.height="2.7vw"
