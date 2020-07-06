94d6ff95bf1dad932f9f2819a04ce93ed805729e
adh.rishabh <adh.rishabh@outlook.com>
2020-07-06 12:05:42 +0530

    start editing for phone

---
+++ download.js
@@ -0,0 +1,8 @@
+
+if( /Android/i.test(navigator.userAgent) ) {
+	window.location.href = 'https://play.google.com/store/apps/details?id=com.openly.forconsumer';	
+}
+
+if( /iPhone/i.test(navigator.userAgent) ) {
+	window.location.href = 'https://apps.apple.com/in/app/openly-marketplace/id1519469761';
+}
--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 
-if( /Android/i.test(navigator.userAgent) ) {
-	window.location.href = 'https://play.google.com/store/apps/details?id=com.openly.forconsumer';	
-}
-
-if( /iPhone/i.test(navigator.userAgent) ) {
-	window.location.href = 'https://apps.apple.com/in/app/openly-marketplace/id1519469761';
-}
 
 
 
