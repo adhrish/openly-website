f96ed32f010eb6a467f5236340655c9e978c1e0c
adh.rishabh <adh.rishabh@outlook.com>
2020-07-08 23:21:17 +0530

    animation done

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
+function downloadcount(element, target){
+	if(document.body.scrollTop>=80 && element.getAttribute('animated') == "false"){
+			function counter(i=0) {
+			if(i==target){
+				element.setAttribute('animated', true);
+				element.innerHTML= target;
+			} else {
+ 				element.innerHTML= i;
+				setTimeout(() => {
+					counter(++i);
+				}, 0);
+			}		
+		 }counter();
+	}		
+}
 
+function animateCount() {
+	const response = JSON.parse(localStorage.getItem('downloadCounts'));
+	const items = [
+		{
+			element: document.getElementById("userpotnum"),
+			target: response.users,
+		},
+		{
+			element: document.getElementById("vendorpotnum"),
+			target: response.stores,
+		},
+		{
+			element: document.getElementById("orderpotnum"),
+			target: response.orders,
+		}];
+	for(let i = 0; i < items.length; i++) {
+		downloadcount(items[i].element, items[i].target);
+	}
+}
+
+document.addEventListener('scroll', animateCount);
 
 function scrollHeader() {
 		if(document.body.scrollTop <= 40){
@@ -14,5 +50,37 @@ function scrollHeader() {
 			document.getElementById("header-logo").style.height="2.7vw"
 			
 		}
-}	
+}
 
+
+
+
+// api call
+function getDownloadCount() {
+
+	const options = {
+		method: 'get',
+	};
+	// cordova.plugin.http.sendRequest(APIURL+'/api/0OnapDD7qF4ULklkfQLJDGnqAKlbHqD2mMuNwVoOuCz7sRLunX/get_stats', options,
+	// function(response){
+	// 	localStorage.setItem('downloadCounts', response.data);
+	// },
+	// function(response) {
+	// 	console.log(response)
+	// 	toggleapierroralert("couldn't get your details")
+	// 	end_loading()
+	// });
+
+	fetch('https://openly-test-heroku.herokuapp.com/api/0OnapDD7qF4ULklkfQLJDGnqAKlbHqD2mMuNwVoOuCz7sRLunX/get_stats')
+	.then(function(response) {
+		return response.json();
+	}).then(function(response) {
+		localStorage.setItem('downloadCounts', JSON.stringify(response));
+	})
+	.catch(function(error){
+		console.log(error);
+	})
+}
+
+
+getDownloadCount();
