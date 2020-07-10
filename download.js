0fc43b32f9a3aadee22866f0150a7aec76fad1dc
adh.rishabh <adh.rishabh@outlook.com>
2020-07-10 17:15:36 +0530

    update changes

--- download.js
+++
@@ -1,8 +0,0 @@
-
-if( /Android/i.test(navigator.userAgent) ) {
-	window.location.href = 'https://play.google.com/store/apps/details?id=com.openly.forconsumer';	
-}
-
-if( /iPhone/i.test(navigator.userAgent) ) {
-	window.location.href = 'https://apps.apple.com/in/app/openly-marketplace/id1519469761';
-}
--- index.js
+++ index.js
@@ -38,13 +38,11 @@ document.addEventListener('scroll', animateCount);
 
 function scrollHeader() {
 		if(document.body.scrollTop <= 40){
-		document.getElementById("logotextopenly").style.fontSize= "1.8vw";
 		document.getElementById("header").style.height = "8vw";
 		document.getElementById("header").style.boxShadow="none";
 		document.getElementById("header-logo").style.height="5vw";
 		}
 		else if (document.body.scrollTop >= 40){
-			document.getElementById("logotextopenly").style.fontSize= "0.9vw";
 			document.getElementById("header").style.height = "3.5vw";
 			document.getElementById("header").style.boxShadow="0 0 1vw rgba(0,0,0,0.05)";
 			document.getElementById("header-logo").style.height="2.7vw"
