cec1236f2e5591db0f2aff2831db184a77000c8a
adh.rishabh <adh.rishabh@outlook.com>
2020-07-12 00:16:50 +0530

    first consumer page commit

--- index.js
+++ index.js
@@ -36,19 +36,7 @@ function animateCount() {
 
 document.addEventListener('scroll', animateCount);
 
-function scrollHeader() {
-		if(document.body.scrollTop <= 40){
-		document.getElementById("header").style.height = "8vw";
-		document.getElementById("header").style.boxShadow="none";
-		document.getElementById("header-logo").style.height="5vw";
-		}
-		else if (document.body.scrollTop >= 40){
-			document.getElementById("header").style.height = "3.5vw";
-			document.getElementById("header").style.boxShadow="0 0 1vw rgba(0,0,0,0.05)";
-			document.getElementById("header-logo").style.height="2.7vw"
-			
-		}
-}
+
 
 
 
