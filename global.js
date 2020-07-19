e997fbaf60a67b5266221acab6a046a3a94e8f2e
adh.rishabh <adh.rishabh@outlook.com>
2020-07-19 16:34:57 +0530

    design business page and minor change in consumer page

--- global.js
+++ global.js
@@ -1,10 +1,13 @@
 function scrollHeader() {
 	if(document.body.scrollTop <= 40){
+		console.log("bro");
 	document.getElementById("header").style.height = "8vw";
 	document.getElementById("header").style.boxShadow="none";
 	document.getElementById("header-logo").style.height="5vw";
 	}
-	else if (document.body.scrollTop >=40){
+	else if (document.body.scrollTop >40){
+
+		console.log("bro1");
 		document.getElementById("header").style.height = "3.5vw";
 		document.getElementById("header").style.boxShadow="0 0 1vw rgba(0,0,0,0.05)";
 		document.getElementById("header-logo").style.height="2.7vw"
