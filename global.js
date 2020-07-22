5b1395961791ea17d968ce209c44617ccd19ed1c
adh.rishabh <adh.rishabh@outlook.com>
2020-07-22 22:43:02 +0530

    modify consumer-app-page

--- global.js
+++ global.js
@@ -1,16 +1,17 @@
 function scrollHeader() {
-	if(document.body.scrollTop <= 40){
-		console.log("bro");
-	document.getElementById("header").style.height = "8vw";
-	document.getElementById("header").style.boxShadow="none";
-	document.getElementById("header-logo").style.height="5vw";
-	}
-	else{
-
-		console.log("bro1");
+	const ele = document.querySelector("header .header-items #header-logo");
+	if(document.body.scrollTop >= 40){
 		document.getElementById("header").style.height = "3.5vw";
 		document.getElementById("header").style.boxShadow="0 0 1vw rgba(0,0,0,0.05)";
-		document.getElementById("header-logo").style.height="2.7vw"
+		ele.style.height="3vw";
+		// document.getElementById("header-logo").style.height="2.7vw"
 		
 	}
+	else{
+	document.getElementById("header").style.height = "8vw";
+	document.getElementById("header").style.boxShadow="none";
+	ele.style.height="5vw";
+	// document.getElementById("header-logo").style.height="5vw";
+	}
+
 }
